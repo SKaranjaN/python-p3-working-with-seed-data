904a2ded990b107801381e3bb7cba999bade36b4
Stephen <skaranja654@gmail.com>
2023-05-29 19:58:46 +0300

    Completed assigment

--- lib/models.py
+++ lib/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -19,3 +20,4 @@ class Game(Base):
         return f'Game(id={self.id}, ' + \
             f'title="{self.title}", ' + \
             f'platform="{self.platform})"'
+    
